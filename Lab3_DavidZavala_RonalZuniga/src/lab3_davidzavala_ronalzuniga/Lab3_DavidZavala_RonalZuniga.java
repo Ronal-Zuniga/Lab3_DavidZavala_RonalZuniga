84d0f7fd217003fcfd012a0c5bf065f440aa46f4
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-10-30 14:22:09 -0600

    Menu remastered

--- Lab3_DavidZavala_RonalZuniga/src/lab3_davidzavala_ronalzuniga/Lab3_DavidZavala_RonalZuniga.java
+++ Lab3_DavidZavala_RonalZuniga/src/lab3_davidzavala_ronalzuniga/Lab3_DavidZavala_RonalZuniga.java
@@ -1,5 +1,6 @@
 package lab3_davidzavala_ronalzuniga;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab3_DavidZavala_RonalZuniga {
@@ -8,13 +9,67 @@ public class Lab3_DavidZavala_RonalZuniga {
 
     public static void main(String[] args) {
         boolean Flag = true;
+        boolean Login = false;
+        boolean Sudo = false;
         while (Flag == true) {
-            System.out.println("");
-            System.out.println("");
+            System.out.println("Equipo Dinavodka");
+            System.out.println("1. Log in");
+            System.out.println("2. Crear Usuario");
+            System.out.println("3. Comprar");
+            System.out.println("4. Log out");
+            System.out.println("5. Administración");
             System.out.println("0. Salir");
             int op = read.nextInt();
             switch (op) {
                 case 1: {
+                    System.out.println("Usuario:");
+                    String Usuario = read.next();
+                    System.out.println("Contraseña:");
+                    String Contraseña = read.next();
+                    if (Usuario.equals("SUDO") && Contraseña.equals("")) {
+                        Sudo = true;
+                    } else if (Usuario.equals("SUDO") && Contraseña.equals("")) { // modificar para las personas normales
+                        Login = true;
+                    } else {
+                        System.out.println("Perfil inexistente intentelo de nuevo");
+                        System.out.println("Usuario:");
+                        Usuario = read.next();
+                        System.out.println("Contraseña:");
+                        Contraseña = read.next();
+                    }
+                    break;
+                }
+                case 2: {
+                    
+                    break;
+                }
+                case 3: {
+                    
+                    break;
+                }
+                case 4: {
+                    
+                    break;
+                }
+                case 5: {
+                    
+                    if (Sudo == true) {
+                        System.out.println("A. Locales");
+                        System.out.println("B. Personas");
+                        System.out.println("C. Productos");
+                        System.out.println("X. Atrás");
+                        System.out.print("Opcion: ");
+                        char oop = read.next().toUpperCase().charAt(0);
+                        switch (oop){
+                            case 'A':
+                            case 'C':
+                            case 'D':
+                            case 'X':
+                            default :
+                        }
+                    }else{
+                        System.out.println("Credenciales necesarias");
+                    }
                     break;
                 }
                 case 0: {
@@ -25,6 +80,12 @@ public class Lab3_DavidZavala_RonalZuniga {
                     System.out.println("Opción inválida \nIntentelo nuevamente");
                 }
             }
+        }
+    }
+
+    static void Print(ArrayList array) {
+        for (int i = 0; i < array.size(); i++) {
+            System.out.println("[" + i + "] {{" + array.get(i) + "}");
         }
     }
 
